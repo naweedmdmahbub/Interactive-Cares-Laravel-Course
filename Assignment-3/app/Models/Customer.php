892ff49602d03562e562630aaf6578dcd8b2da41
naweedmdmahbub <naweedmdmahbub@gmail.com>
2023-10-30 00:25:14 +0600

    Portfolio initialized

---
+++ Assignment-3/app/Models/Customer.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Models;
+
+class Customer {
+  public function login() : string 
+  {
+    
+  }
+}
+
+
+
+?>
--- Assignment-3/index.php
+++ Assignment-3/index.php
@@ -4,18 +4,27 @@
   $income = 0;
   $expense = 0;
   require_once './vendor/autoload.php';
-  use App\Models\Category;
+  use App\Models\User;
+  use App\Models\Customer;
 
   do {
     echo "Select from the following menu".PHP_EOL."1. Login ".PHP_EOL."2. Register".PHP_EOL."3. Exit".PHP_EOL;
     echo "Enter a value: ";
-    $choice = readline("Enter a value (1-3): ");
+    $choice = readline("Enter your choice (1-3): ");
 
     switch ($choice) {
       case 1:
-        $category = new Category();
+        $user = new User();
+        $username = readline("Please enter your username: ");
+        $password = readline("Please enter your password: ");
+
         break;
       case 2:
+        $customer = new Customer();
+        // $customer->register();
+        $email = readline("Email: ");
+        $password = readline("Name: ");
+        $password = readline("Password: ");
         break;
       case 3:
         echo "Exiting the program.".PHP_EOL;
